2646a7cf28a821e808d9d93094da95928862ed8e
luisfs <contato.luisf@gmail.com>
2020-02-04 06:07:56 -0300

    add cors and allow external access

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes')
+const cors = require('cors');
 
 const app = express();
 mongoose.connect('mongodb+srv://admin:ab1234fecd@cluster0-yegig.mongodb.net/week10?retryWrites=true&w=majority', {
@@ -27,6 +28,7 @@ mongoose.connect('mongodb+srv://admin:ab1234fecd@cluster0-yegig.mongodb.net/week
      useUnifiedTopology: true
     });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes)
 
